472b003b160449967d5495c30f61d069a141f38e
egoru <dezertir.106245@gmail.com>
2025-06-29 22:26:45 +0500

    update 4.32

--- src/main/java/com/diplom/diplom/service/videocalls/ServiceVideocallsChatAsync.java
+++ src/main/java/com/diplom/diplom/service/videocalls/ServiceVideocallsChatAsync.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
